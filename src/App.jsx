b4d94d652ae3be62e9e3957d4b94680e56fdf6b4
Vijayeswar-Yalamanchili <vijayeswar_y@yahoo.com>
2024-12-27 16:56:39 +0530

    Update apiurl

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import AppRoutes from './utils/AppRoutes'
 import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 
-export const API_URL =  "https://6597a585668d248edf231e3d.mockapi.io/AxiosTask";
+// export const API_URL =  "https://6597a585668d248edf231e3d.mockapi.io/AxiosTask"
+export const API_URL =  "https://676e8c6fdf5d7dac1ccaff9a.mockapi.io/users"
 
 function App() {
 
